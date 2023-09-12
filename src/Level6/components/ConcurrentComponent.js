656cb7a7a1629afd9481f9520c4263653d8d7648
jagun1813 <jagun@1813studios.com>
2023-09-11 18:45:03 -0600

    level 6 q1-3

--- src/Level6/components/ConcurrentComponent.js
+++ src/Level6/components/ConcurrentComponent.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 
+
 function ConcurrentComponent() {
-    const [count, setCount] = useState(0);
+    const [state, setState] = useState(0);
+    const [isPending, startTransition] = React.useTransition();
 
     return (
         <div>
-            <p>Count: {count}</p>
-            <button className="bg-blue-500 text-white p-2 rounded" onClick={() => setCount(count + 1)}>
-                Increment Count
+            <button className="bg-blue-500 text-white p-2 rounded"
+                onClick={() => {
+                    startTransition(() => {
+                        setState(prevState => prevState + 1);
+                    });
+                }}
+                disabled={isPending}
+            >
+                Increment
             </button>
+            <p>State: {state}</p>
+            {isPending && <p>Loading...</p>}
         </div>
     );
 }
+
+
 
 export default ConcurrentComponent;
--- src/Level6/question1.js
+++ src/Level6/question1.js
@@ -5,6 +5,25 @@ function Question1() {
     return (
         <div className="p-4">
             <h2 className="text-lg font-bold">Implementing Concurrent Rendering in React:</h2>
+            <p>To implement concurrent rendering in a React app, you would use ReactDOM.createRoot to create a root and then render your application with concurrent features enabled. Inside your components, you can use concurrent features such as React.useTransition to create transitions that allow React to work on multiple tasks simultaneously, improving the responsiveness of your app. Here is how you can do it:</p>
+            <ul>
+                <li>First, ensure you are using React 18 or later, as concurrent features are introduced in React 18.</li>
+                <li>In your index file where you mount your React application, use ReactDOM.createRoot instead of ReactDOM.render to create a root with concurrent features enabled:
+                </li>
+            </ul>
+             <pre className="bg-gray-800 text-green-400 p-2 rounded mt-2">
+               {` 
+                import React from 'react';
+                import ReactDOM from 'react-dom';
+                import Question1 from './Question1'; // Adjust the path to your Question1 component
+
+                const root = ReactDOM.createRoot(document.getElementById('root'));
+                root.render(Question1 )
+`}
+            </pre>
+            <div>
+                <p></p>
+            </div>
             <ConcurrentComponent />
             <div className="mt-4 bg-yellow-200 p-4 rounded-md">
                 <h2 className="text-lg font-bold">Fun Fact:</h2>
